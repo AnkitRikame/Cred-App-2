41c046c214e036a38e42618ee1912f036b56240b
Ankit <ankitrikame23@gmail.com>
2021-05-31 20:54:32 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -67,36 +67,7 @@ const App = () => {
 		setAllEnt([...allEnt, newData]);
 		setData(newData);
 		console.log(newData);
-
-		// const newData = { ...data };
-		// newData[e.target.id] = e.target.value;
 	};
-
-	// 2nd option .....................
-	// const submitData = (e) => {
-	// 	e.preventDefault();
-
-	//   const newData = {
-
-	// if (number && name && expiry && cvc) {
-	// 	const newEnt = {
-	// 		number: number,
-	// 		name: name,
-	// 		expiry: expiry,
-	// 		cvc: cvc,
-	// 		focus: focus,
-	// 	};
-	// 	setAllEnt([...allEnt, newEnt]);
-
-	// 	setNumber("");
-	// 	setName("");
-	// 	setExpiry("");
-	// 	setCvc("");
-	// } else {
-	// 	alert("please fill the data properly");
-	// }
-	// console.log(allEnt);
-	// };
 
 	return (
 		<>
@@ -129,10 +100,6 @@ const App = () => {
 							onChange={(e) => setNumber(e.target.value)}
 							onFocus={(e) => setFocus(e.target.name)}
 							required
-							// min="1"
-							// max="16"
-							// maxLength="16"
-							// size="20"
 						/>
 						<br />
 
@@ -145,8 +112,6 @@ const App = () => {
 							onChange={(e) => setName(e.target.value)}
 							onFocus={(e) => setFocus(e.target.name)}
 							required
-							// min="1"
-							// maxLength="20"
 						/>
 						<br />
 
@@ -157,8 +122,6 @@ const App = () => {
 							min="0"
 							placeholder=" MM/YY Expiry"
 							value={expiry}
-							// min="0"
-							// max="4"
 							onChange={(e) => setExpiry(e.target.value)}
 							onFocus={(e) => setFocus(e.target.name)}
 							required
@@ -174,8 +137,6 @@ const App = () => {
 							onChange={(e) => setCvc(e.target.value)}
 							onFocus={(e) => setFocus(e.target.name)}
 							required
-							// min="0"
-							// max="3"
 						/>
 						<br />
 						<button className="submit_button" type="submit">
